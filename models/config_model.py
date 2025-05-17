3f9961f04ea52180fc16d40c072bb2bc98f9080e
Popov Egor <popoveg18@gmail.com>
2025-05-17 19:09:35 +0300

    Refactoring config model

--- models/config_model.py
+++ models/config_model.py
@@ -1,4 +1,3 @@
-import subprocess
 from pathlib import Path
 
 class ConfigModel:
@@ -15,29 +14,6 @@ class ConfigModel:
                  if file.is_file()]
         return list(map(lambda file: file.removesuffix(self.extension),
                         files))
-
-    def run_connect_command(self, config_file):
-        result = subprocess.run(
-            ['pkexec', 'ls', '/home/osc_circuit/Documents/CodeProjects'],
-            # check=True,
-            stderr=subprocess.DEVNULL,  # Полное игнорирование stderr
-            stdout=subprocess.PIPE,     # Если нужно перехватить stdout
-            # capture_output=True,
-            text=True
-        )
-        # print(result.stdout)
-        self.current_use_config = config_file
-        response = {'status': 'connect',
-                    'message': f'Connection has been created by {config_file}'}
-        return response
-
-    def run_disconnect_command(self):
-        response = {'status': 'disconnect',
-                    'message': 'Connection has been closed by ' \
-                               f'{self.current_use_config}',
-                   }
-        self.current_use_config = None
-        return response
 
     def get_preety_configs(self):
         return self.configs
